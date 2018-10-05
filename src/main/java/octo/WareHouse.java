103b91378cbc5658944e5473c92faa1e34efa6eb
Aditi Phadke <aditihphadke@gmail.com>
2018-10-05 11:28:13 +1000

    Update WareHouse.java

--- src/main/java/octo/WareHouse.java
+++ src/main/java/octo/WareHouse.java
@@ -7,16 +7,6 @@ import java.util.List;
 
 public class WareHouse {
 
-    private GSTCalculator gstCalculator;
-    private ProductRepository productRepository;
-    private EmailService emailService;
-
-    public WareHouse() {
-        gstCalculator = new GSTCalculator();
-        productRepository = new ProductRepository();
-        emailService = new EmailService();
-    }
-
     public void processProducts(String user, List<Product> products) {
         for (Product product : products) {
             // calculate gst price gstCalculator
